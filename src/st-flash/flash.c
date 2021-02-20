325490e40b2133db1284c6f16bb82d38b7813abf
Dominggus Salampessy <dominggus@scentronix.com>
2021-02-20 22:56:20 +0100

    added (domingguss) for debugging purposes ofc

--- src/st-flash/flash.c
+++ src/st-flash/flash.c
@@ -59,7 +59,7 @@ int main(int ac, char** av) {
         return(-1);
     }
 
-    printf("st-flash %s\n", STLINK_VERSION);
+    printf("st-flash %s (domingguss)\n", STLINK_VERSION);
 
     sl = stlink_open_usb(o.log_level,
             o.connect_under_reset ? 2 : 1,
